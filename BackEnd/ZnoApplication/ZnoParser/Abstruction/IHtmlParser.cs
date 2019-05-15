28d270c1d8a1628579677fe62e81fb3e8a37887e
Nikita Bykov <nikita.bykov.1996@gmail.com>
2019-05-15 15:04:09 +0300

    Создание ZnoParser

---
+++ BackEnd/ZnoApplication/ZnoParser/Abstruction/IHtmlParser.cs
@@ -0,0 +1,12 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ZnoParser.Abstruction
+{
+    interface IHtmlParser
+    {
+        void InitByHtmlNode(HtmlNode node);
+    }
+}
