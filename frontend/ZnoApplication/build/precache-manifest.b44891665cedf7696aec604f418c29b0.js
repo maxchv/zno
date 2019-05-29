c7a5eedd5c40bcd67e8b1d79e75510cf66529cf8
Maxim <maxshaptala@gmail.com>
2019-05-29 15:17:33 +0300

    build frontend

--- frontend/ZnoApplication/build/precache-manifest.b44891665cedf7696aec604f418c29b0.js
+++ frontend/ZnoApplication/build/precache-manifest.b44891665cedf7696aec604f418c29b0.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d2f3a0a798e0e593ab2e772120826577",
+    "revision": "c91b52f7cb8fa4d17d1f15be35485413",
     "url": "/index.html"
   },
   {
-    "revision": "eec802fec0148b7e00d3",
+    "revision": "295b0d0807ce4dfa413a",
     "url": "/static/css/main.8f4974f7.chunk.css"
   },
   {
-    "revision": "cb52e07424d925c074c3",
-    "url": "/static/js/2.42055525.chunk.js"
+    "revision": "619ef5c53ce0c8058c92",
+    "url": "/static/js/2.d1c04301.chunk.js"
   },
   {
-    "revision": "eec802fec0148b7e00d3",
-    "url": "/static/js/main.b49c285d.chunk.js"
+    "revision": "295b0d0807ce4dfa413a",
+    "url": "/static/js/main.d6fa72ec.chunk.js"
   },
   {
     "revision": "42ac5946195a7306e2a5",
